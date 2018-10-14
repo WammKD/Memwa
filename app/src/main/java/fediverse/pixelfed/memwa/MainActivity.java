33e90f2abae7a6a6e37dd36c78e7d8b68ca61151
Wamm K. D <jaft.r@outlook.com>
2018-10-13 23:54:36 -0500

    Just making things more homely.

--- app/src/main/java/fediverse/pixelfed/memwa/MainActivity.java
+++ app/src/main/java/fediverse/pixelfed/memwa/MainActivity.java
@@ -2,43 +2,41 @@ package fediverse.pixelfed.memwa;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+	private TextView                         mTextMessage;
+	private OnNavigationItemSelectedListener myONISL      = new OnNavigationItemSelectedListener() {
+			@Override
+			public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+				switch(item.getItemId()) {
+					case R.id.navigation_home:
+						mTextMessage.setText(R.string.title_home);
+						return true;
+					case R.id.navigation_dashboard:
+						mTextMessage.setText(R.string.title_dashboard);
+						return true;
+					case R.id.navigation_notifications:
+						mTextMessage.setText(R.string.title_notifications);
+						return true;
+				}
 
-    private TextView mTextMessage;
+				return false;
+			}
+		};
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+		mTextMessage                    = (TextView)             findViewById(R.id.message);
+		BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
-
+		navigation.setOnNavigationItemSelectedListener(myONISL);
+	}
 }
